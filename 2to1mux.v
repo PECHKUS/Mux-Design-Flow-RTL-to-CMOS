92d17fd9ac2e1bba4e9bb9cb9989f2d3b2781297
Parnavo Pal <145892746+PECHKUS@users.noreply.github.com>
2026-01-10 15:03:00 +0530

    Update 2to1mux.v

--- 2to1mux.v
+++ 2to1mux.v
@@ -1,10 +1,19 @@
+`timescale 1ns/1ps
 
+module mux2to1 (
+    input  I0,
+    input  I1,
+    input  S,
+    output Out
+);
 
-module mux2to1(in, sel, out);
-	input[1:0] in;
-	input sel;
-	output out;
+    wire S_bar;
 
-	assign out = in[sel];
+    
+	not (S_bar, S);  // inverter for select
+
+    // Pass transistor logic
+	tranif0 T0 (Out, I0, S);     // when S = 0 → pass I0
+	tranif1 T1 (Out, I1, S);     // when S = 1 → pass I1
 
 endmodule
