6f27d1bc0f548b3ec76bd477ccbb677643197f96
Parnavo Pal <145892746+PECHKUS@users.noreply.github.com>
2026-01-10 15:07:37 +0530

    Update tb_Mux_16x1.v

--- tb_Mux_16x1.v
+++ tb_Mux_16x1.v
@@ -4,28 +4,41 @@
 
 module muxtest;
 
-    reg [15:0] A;
-    reg [3:0] S;
+    reg  [15:0] A;
+    reg  [3:0]  S;
     wire F;
+
+    integer i;
 
     mux16to1 M (.in(A), .sel(S), .out(F));
 
-    initial
-    begin
-        // Setup Dump Files
-        $dumpfile("mux16to1.vcd");
-        $dumpvars(0,muxtest);
-        
-        // Add Header to the console output
-        $display("Time | A (Input) | S (Select) | F (Output)");
-        $monitor ($time, "   | %h        | %h         | %b", A, S, F);
+    initial begin
+        $dumpfile("mux16to1_ptl.vcd");
+        $dumpvars(0, muxtest);
 
-        // Initialization 
-        #5 A = 16'h3f0a; S = 4'h0;
-	#5 S = 4'h1;
-	#5 S = 4'h6;
-	#5 S = 4'hc;
-	#5
+        $display("Time | A | S | F");
+
+        // 1st input pattern
+        A = 16'h3F0A;
+        for (i = 0; i < 16; i = i + 1) begin
+            S = i;
+            #10;
+        end
+
+        // second input pattern
+        A = 16'hA5A5;
+        for (i = 0; i < 16; i = i + 1) begin
+            S = i;
+            #10;
+        end
+
         $finish;
     end
+
+    // Detect floating output (important for PTL)
+    always @(*) begin
+        if (F === 1'bz)
+            $display("WARNING: Output floating at time %0t", $time);
+    end
+
 endmodule
